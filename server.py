947e6f06cf5231cadb848ff204573a31058c401d
augmnt <plymth@protonmail.com>
2026-01-13 00:46:46 +0100

    fix: conditionally skip host/port config for fastmcp.cloud

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 """Entry point for fastmcp.cloud deployment."""
+import os
 import sys
+
+# Signal to server.py that we're running in fastmcp.cloud
+os.environ["FASTMCP_CLOUD"] = "true"
 
 sys.path.insert(0, "src")
 from augments_mcp.server import mcp
